d7eb375d3c75aacc53cdb2701b53c70241e71077
benjaminkent <benjamin.k.jehl@gmail.com>
2019-03-16 14:03:57 -0400

    initial commit, config updated

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,17 @@
-<template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
-  </div>
+<template lang="pug">
+  .home-container
+    Header
+    img(src="@/assets/logo.png")
 </template>
 
 <script lang="ts">
 import Vue from "vue";
-import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
+import Header from "@/components/Header.vue"
 
 export default Vue.extend({
   name: "home",
   components: {
-    HelloWorld
+    Header
   }
 });
 </script>
